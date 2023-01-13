24ee3cbfcfdf97ce75532b2565d2bea88edc9b09
hamzajoshan <hamzajoshan@gmail.com>
2023-01-13 10:55:57 +0500

    filter in dropdown

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,29 +1,45 @@
-import * as React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { AppContext } from '../context';
-import {useNavigate} from 'react-router-dom'
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { AppContext } from "../context";
+import { useNavigate } from "react-router-dom";
 export default function SelectVariants() {
-  const [filter, setFilter] = React.useState('home');
-  const navigate=useNavigate();
+  const [filter, setFilter] = React.useState("home");
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    const uri = window.location.href;
+    const splittedURI = uri.split("/");
+    // if(splittedURI.includes('all-branches') || splittedURI.includes('all-states') || splittedURI.includes('all-cities') || splittedURI.includes('get-cites-from-states') || splittedURI.includes('get-branches-from-city') || splittedURI.includes('branch-details')){
+    //   console.log('yes its location filter')
+    // }
+    if (splittedURI.includes("all-branches")) {
+      console.log("yes its location filter");
+      setFilter("locations");
+    } else if (
+      splittedURI.includes("all-states") ||
+      splittedURI.includes("all-cities") ||
+      splittedURI.includes("get-cites-from-states") ||
+      splittedURI.includes("get-branches-from-city") ||
+      splittedURI.includes("branch-details")
+    ) {
+      console.log("yes its location filter");
+      setFilter("locations");
 
+    }
+  }, []);
   const handleChange = (event) => {
     setFilter(event.target.value);
-    if(event.target.value==='locations'){
-      localStorage.setItem('filter', event.target.value)
-      navigate('/pages/all-branches')
-
-    }else if(event.target.value==='home'){
-      localStorage.setItem('filter', event.target.value)
-      navigate('/')
+    if (event.target.value === "locations") {
+      navigate("/pages/all-branches");
+    } else if (event.target.value === "home") {
+      navigate("/");
     }
-    console.log('locations', event.target.value)
-   
-
+    console.log("locations", event.target.value);
   };
- const filterV = localStorage.getItem('filter')
+  const filterV = localStorage.getItem("filter");
+  console.log({ filterV });
   return (
     <div>
       <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
@@ -31,12 +47,17 @@ export default function SelectVariants() {
         <Select
           labelId="demo-simple-select-standard-label"
           id="demo-simple-select-standard"
-          value={filterV || filter}
+          value={filter}
           onChange={handleChange}
           label=""
         >
-          <MenuItem key={1} value={"home"}>Home</MenuItem>
-          <MenuItem key={2} value={"locations"}>Locations</MenuItem>
+          <MenuItem key={1} value={""}></MenuItem>
+          <MenuItem key={1} value={"home"}>
+            Home
+          </MenuItem>
+          <MenuItem key={2} value={"locations"}>
+            Locations
+          </MenuItem>
         </Select>
       </FormControl>
     </div>
